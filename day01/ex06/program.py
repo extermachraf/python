4f178de9f77a584b83d22a7cac0de1d0437ff4fc
extermachraf <ael-kouc@student.1337.ma>
2025-06-08 21:36:24 +0100

    (update): how it work ex06

--- day01/ex06/program.py
+++ day01/ex06/program.py
@@ -3,19 +3,17 @@ import sys
 import re 
 
 
-def parseArgs(args: list) -> tuple[list, int]:
+def parseArgs(args: list) -> tuple[list[str], int]:
     try:
         if len(args) != 3:
-            raise AssertionError("the second args in arg should be a number")
+            raise AssertionError("Program should receive exactly 2 arguments")
         counter: int = int(args[2])
-        isNotSafe: bool = bool(re.fullmatch(r"[A-Za-z0-9 ]", args[1]))
+        isNotSafe: bool = not bool(re.fullmatch(r"[A-Za-z0-9 ]*", args[1]))
         if isNotSafe:
             raise AssertionError("the first arg should not containe a special caracters att all")
         return [args[1].split(), counter]
     except ValueError as e:
-        raise AssertionError("the second args in arg should be a number")
-    except AssertionError as e:
-        raise AssertionError(e)
+        raise AssertionError("the second arg should be a number")
         
 
 def main():
