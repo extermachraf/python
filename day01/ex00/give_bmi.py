89663483199e9ede75b79bcc7560012fd8d1c9f5
extermachraf <ael-kouc@student.1337.ma>
2025-06-20 22:18:49 +0100

    up

---
+++ day01/ex00/give_bmi.py
@@ -0,0 +1,18 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    if len(height) != len(weight):
+        raise AssertionError("lists should be in the same height")
+    if any (h == 0 for h in height):
+        raise AssertionError("l3adam ya dinek")
+        
+    if not all(isinstance(w, (int, float)) for w in weight):
+        raise AssertionError("weight should containe only numbers")
+    if not all(isinstance(h, (int, float)) for h in height):
+        raise AssertionError("height should containe only numbers")
+    BMi : list = []
+    for i in range(len(height)):
+        BMi.append(weight[i] / (height[i] ** 2))
+    return BMi
+        
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    return [b > limit for b in bmi]
---
+++ day01/ex00/main.py
@@ -0,0 +1,11 @@
+import give_bmi
+
+
+def main():
+    BMI = give_bmi.give_bmi([2.71, 1.15], [165.3, 38.4])
+    print(BMI)
+    limit = give_bmi.apply_limit(BMI, 26)
+    print(limit)
+    
+if __name__ == "__main__":
+    main()
